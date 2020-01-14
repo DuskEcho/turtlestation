4fcc57687e0eb692d59a3f9b8079dae87f8b17b3
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2020-01-14 09:58:33 -0800

    in progress conversion from stationary to live

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const raspividStream = require('raspivid-stream');
+const wss = require('express-ws')(app);
 const app = express();
 const controller = require('./controllers/controller.js');
 
@@ -8,7 +9,28 @@ app.set('port', process.env.PORT || '8081');
 app.set('ip', process.env.IP || '0.0.0.0');
 app.use(express.static('public'));
 app.get('/', (req, res) => res.render('home'));
-app.ws('/stream', controller.grabStream);
+
+app.ws('/stream', (ws, req) => {
+  console.log('Incoming!');
+  controller.turnOnIR();
+
+  ws.send(JSON.stringify({
+    action: 'init',
+    width: '960',
+    height: '540'
+  }));
+
+    var videoStream = raspividStream({ rotation: 0 });
+    videoStream.on('data', (data) => {
+        ws.send(data, { binary: true }, (error) => { if (error) console.log(error); });
+    });
+
+    ws.on('close', () => {
+        console.log('Later!');
+        controller.turnOffIR();
+        videoStream.removeAllListeners('data');
+    });
+});
 
 
 app.listen(app.get('port'), app.get('ip'), ()=>console.log(`Express server running at ${app.get('ip')} on port ${app.get('port')}`));
