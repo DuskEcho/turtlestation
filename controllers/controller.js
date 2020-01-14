4e4b327a3255708f71466a055c8f7a3ebc4df089
DuskEcho <42277980+DuskEcho@users.noreply.github.com>
2020-01-14 09:52:17 -0800

    In progress conversion from stationary to live

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,5 +1,7 @@
 const PiCamera = require('pi-camera');
 const picPath = `../public/img/tempPic.jpg`;
+const raspividStream = require('raspivid-stream');
+const wss = require('express-ws')(app);
 const cam = new PiCamera({
 	mode: 'photo',
 	output: picPath,
@@ -13,6 +15,39 @@ module.exports = {
 	showPic: async  (req, res)=>{
 	await cam.snap();
   	res.sendfile(picPath);
-}
 
-}
+
+	},
+
+	grabStream: (ws, req) => {
+		console.log('Incoming!');
+		turnOnIR();
+		ws.send(JSON.stringify({
+			action: 'init',
+			width: '960',
+			height: '540'
+		}));
+		let videoStream = raspividStream({ rotation: 0 });
+
+
+		videoStream.on('data', (data) => {
+			ws.send(data, { binary: true }, (error) => { if (error) console.log(error); });
+		});
+
+		ws.on('close', () => {
+			console.log('Later!');
+			turnOffIR();
+			videoStream.removeAllListeners('data');
+		});
+	},
+
+
+
+};
+
+turnOnIR = () =>{
+
+};
+turnOffIR = () =>{
+
+};
